ff989cfa2db2a954e05bacd96fa7fb8b1e49147e
Rufus Ajiboye <ruufman@gmail.com>
2024-11-03 10:53:32 +0000

    Added terraform ec2 Instance

--- main.tf
+++ main.tf
@@ -1,4 +1,13 @@
 provider "aws" {
-    region = "eu-west-2"
-  
+  region = "eu-west-2"
+}
+
+resource "aws_instance" "ec2-aws_instance" {
+  ami           = "ami-00385a401487aefa4"
+  instance_type = "t2-micro"
+  key_name      = "mykeys100"
+
+  tags = {
+    Name = "Node Created by Git Actions"
+  }
 }
