53a4f4a32805218ef3c1b68b1dbd583af9a8100a
Rufus Ajiboye <ruufman@gmail.com>
2024-11-03 12:30:58 +0000

    Adding State file

--- main.tf
+++ main.tf
@@ -13,14 +13,11 @@ resource "aws_instance" "node1" {
   }
 }
 
-
-resource "aws_instance" "node2" {
-  instance_type               = "t2.micro"
-  ami                         = "ami-00385a401487aefa4"
-  key_name                    = "mykeys100"
-  associate_public_ip_address = true
-
-  tags = {
-    Name = "New Node Created by Git Actions"
-  }
+terraform {
+  
+    backend "s3" {
+        bucket = "techbleat-terraform-storage"
+        key = "action/dev/terraform.tfstate"
+        region = "eu-west-1"
+    }
 }
