4b088f6ad6b3b86bdc3c55366f97c989edc64e89
Rufus Ajiboye <ruufman@gmail.com>
2024-11-03 11:23:47 +0000

    Added env variable for  ec2 Instance

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "node1" {
   //ami           = "ami-053b46974baed7332"
   ami           = "ami-0b4c7755cdf0d9219"
   instance_type = "t2.micro"
-  key_name      = "mykeys100"
+  //key_name      = "mykeys100"
 
   tags = {
     Name = "Node Created by Git Actions"
