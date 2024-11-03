a8b3133ada146154e9869155a29bc171785793d5
Rufus Ajiboye <ruufman@gmail.com>
2024-11-03 11:46:24 +0000

    Added env variable for  ec2 Instance

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-  region = "eu-west-2"
+  region = "eu-west-1"
 }
 
 resource "aws_instance" "node1" {
-    ami           = "ami-0fed63ea358539e44"
-    instance_type = "t2.micro"
+  instance_type               = "t2.micro"
+  ami                         = "ami-00385a401487aefa4"
+  associate_public_ip_address = true
 
   //ami           = "ami-053b46974baed7332"
-  //ami           = "ami-00385a401487aefa4"
   //key_name      = "mykeys100"
 
   tags = {
