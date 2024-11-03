9ac2e34ffd75b215f48aab75c532a69cef7bc87a
Rufus Ajiboye <ruufman@gmail.com>
2024-11-03 11:15:30 +0000

    Added env variable for  ec2 Instance

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "node1" {
   //ami           = "ami-053b46974baed7332"
-  ami           = "ami-00385a401487aefa4"
+  ami           = "ami-054a53dca63de757b"
   instance_type = "t2.micro"
   key_name      = "mykeys100"
 
